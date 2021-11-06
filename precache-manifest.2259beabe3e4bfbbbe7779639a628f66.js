700011f6fb07aa5a0549065ee6af57353fc9e3a6
Ajithbala <ajithbala50@gmail.com>
2021-11-06 20:02:43 +0530

    Updates

---
+++ precache-manifest.2259beabe3e4bfbbbe7779639a628f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b086fc4a279ea092a516e71e7653efee",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "79b30854c7e46dff475b",
+    "url": "/testapp/static/css/main.44cf70d9.chunk.css"
+  },
+  {
+    "revision": "53cc23f499e22ce64166",
+    "url": "/testapp/static/js/2.35884559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp/static/js/2.35884559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b30854c7e46dff475b",
+    "url": "/testapp/static/js/main.5a4237f7.chunk.js"
+  },
+  {
+    "revision": "a2d6cc884544c67f5eb8",
+    "url": "/testapp/static/js/runtime-main.bda0b417.js"
+  }
+]);
